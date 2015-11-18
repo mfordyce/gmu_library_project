2367a6f799f8975cea079f8feb635b0c3c918c30
Mark Fordyce <mfordyce07@gmail.com>
2015-11-17 19:31:44 -0800

    Through most of Class 2 -- Lab 5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,4 +13,43 @@ class BooksController < ApplicationController
 
   end
 
+  def new
+    @book = Book.new
+  end
+
+
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book
+  end
+
+
+
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to @book
+  end
+
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_url
+  end
+
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :pages, :price)
+  end
 end
